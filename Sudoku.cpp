acb2937f85eacc552fdc1b731d923e6b7927bb9c
e84001032 <e84001032@mail.ncku.edu.tw>
2015-04-18 10:21:23 +0800

    work, but can't find multi-solution help~~~

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -3,283 +3,267 @@ using namespace std;
 
 Sudoku::Sudoku()
 {
-    for(int i = 0 ; i < sudokuSize ; i++){
-        cell[i] = 0;
-    }
+    ans = 0;
+    for(int x = 0 ; x < 9 ; x++)
+        for(int y = 0 ; y < 9; y++)
+            cell[x][y] = 0;
+
 }
 
 void Sudoku::ReadIn(){
-    for(int i = 0 ; i < sudokuSize ; i++){
-        cin >> cell[i];
-    }
+    for(int x = 0 ; x < 9 ; x++)
+        for(int y = 0 ; y < 9 ; y ++)
+            cin >> cell[x][y];
 }
 
 void Sudoku::Solve(){
-    
-    if(Process()){
-	PrintOut();
+    Solving();
+    backtracking(cell);
+    PrintOut();
+}
+
+void Sudoku::FirstCheck(){
+    //check all possibilities for every 0 cell
+    for(int x = 0 ; x < 9 ; x++){
+        for(int y = 0; y < 9; y++){
+            if(cell[x][y] == 0){
+                for(int j = 1 ; j < 10 ; j++){
+                    possibility[x][y][j] = 1;
+                }
+                possibility[x][y][0] = 9;
+            }
+            else{
+                for(int j = 0 ; j < 10 ; j++){
+                    possibility[x][y][j] = 0;
+                }
+            }
+        }
     }
-    else
-	cout << "no solution" << endl;
+    for(int x = 0 ; x < 9 ; x++){
+        for(int y = 0; y < 9 ; y++){
+            if(cell[x][y] == 0){
+                for(int j = 0 ; j < 9 ; j++){
+                    //check row
+                    if(j!=y && cell[x][j] != 0 && cell[x][j] != -1){
+                        if(possibility[x][y][cell[x][j]] == 1){
+                            possibility[x][y][cell[x][j]] -= 1;
+                            possibility[x][y][0] -= 1;
+                        }
+                    }
+                    //check col
+                    if(j!=x && cell[j][y] != 0 && cell[j][y] != -1){
+                        if(possibility[x][y][cell[j][y]] == 1){
+                            possibility[x][y][cell[j][y]] -= 1;
+                            possibility[x][y][0] -= 1;
+                        }
+                    }
+                }
+                for(int n = 0; n < 9; n++){
+                    int xx = x/3*3+n%3;
+                    int yy = y/3*3+n/3;
+                    if(xx!=x && yy!=y && cell[xx][yy]!=0 && cell[xx][yy]!=-1){
+                        if(possibility[x][y][cell[xx][yy]] == 1){
+                            possibility[x][y][cell[xx][yy]] -= 1;
+                            possibility[x][y][0] -= 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void Sudoku::CheckAfterFill(int x,int y){
+    int value = cell[x][y];
+    for(int j = 0 ; j < 10 ; j ++){
+        possibility[x][y][j] = 0;
+    }
+    for(int j = 0 ; j < 9 ; j++){
+        if( possibility[x][j][value] == 1){
+            possibility[x][j][value] -= 1;
+            possibility[x][j][0] -= 1;
+        }
+        if( possibility[j][y][value] == 1){
+            possibility[j][y][value] -= 1;
+            possibility[j][y][0] -= 1;
+        }
+    }
+    for(int n = 0 ; n < 9 ; n++){
+        int xx = x/3*3+n%3;
+        int yy = y/3*3+n/3;
+        if( possibility[xx][yy][value] == 1){
+            possibility[xx][yy][value] -= 1;
+            possibility[xx][yy][0] -= 1;
+        }
+
+    }
 
 }
 
-bool Sudoku::Process(){
+void Sudoku::Solving(){
     FirstCheck();
     while(true){
-	int before = ZeroCount();
-	Solve_strategy1();
-	Solve_strategy2();
-	int after = ZeroCount();
-	if(after == 0) return true;
-	if(after == before) break;
+        int before = ZeroCount();
+        Solve_strategy1();
+        Solve_strategy2();
+        int after = ZeroCount();
+        if(before == after) break;
     }
-    int processing = GetNextZero();
-    for(int i = 1 ; i < 10 ; i ++){
-	if(possibility[processing][i] == 1){
-	    cell[processing] = i;
-	    if(Process()) return true;
-	    cell[processing] = 0;
-	}
-    }
-    return false;
-}
-
-int Sudoku::GetNextZero(){
-    int i;
-    for( i = 0 ; i < sudokuSize ; i ++){
-	if(cell[i] == 0) return i;
-    }
-    return -1;
-}
-
-int Sudoku::ZeroCount(){
-    int zerocount = 0 ;
-    for(int i = 0 ; i < sudokuSize ; i++){
-        if(cell[i] == 0){
-	    zerocount += 1;
-        }	
-    } 
-    return zerocount;
 }
 
 void Sudoku::Solve_strategy1(){
     //check all possibilities for every 0 cell
-    for(int i = 0 ; i < sudokuSize ; i++){
-        if(cell[i] == 0){ 
-	    if(possibility[i][0] == 1){
-		for(int j = 1 ; j < 10 ; j++){
-	    	    if(possibility[i][j] == 1){
-			cell[i] = j;
-			CheckAfterFill(i);
-			break;
-		    }	
-		}		
-	    }
-	}
+    for(int x = 0 ; x < 9 ; x++){
+        for(int y = 0 ; y < 9 ; y++)
+            if(cell[x][y] == 0){
+            if(possibility[x][y][0] == 1){
+                for(int j = 1 ; j < 10 ; j++){
+                    if(possibility[x][y][j] == 1){
+                        cell[x][y] = j;
+                        CheckAfterFill(x,y);
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
-void Sudoku::FirstCheck(){
-    //check all possibilities for every 0 cell
-    for(int i = 0 ; i < sudokuSize ; i++){
-        if(cell[i] == 0){
-	   for(int j = 1 ; j < 10 ; j++){
-		possibility[i][j] = 1;
-            }	
-            possibility[i][0] = 9;
-	}
-	else{
-            for(int j = 1 ; j < 10 ; j++){
-                possibility[i][j] = 0;
-            }
-            possibility[i][0] = 0;
-	} 
-    }
-    for(int i = 0 ; i < sudokuSize ; i++){
-        if(cell[i] == 0){
-            int row = i/9;
-            int col = i%9;
-            int zonerow = i/27;
-            int zonecol = i%9/3;
-            int zone[9] = {0,1,2,9,10,11,18,19,20};
-            for(int j = 0 ; j < 9 ; j++){
-                zone[j] += zonerow * 27 + zonecol * 3;
-            }
-            for(int j = 0 ; j < 9 ; j++){
-                //check row
-                if(cell[row*9+j] != 0){
-		    if(possibility[i][cell[row*9+j]] == 1){
-                        possibility[i][cell[row*9+j]] -= 1;
-                        possibility[i][0] -= 1;
-                    }
-                }
-                //check col
-                if(cell[col+j*9] != 0){
-                    if(possibility[i][cell[col+j*9]] == 1){
-                        possibility[i][cell[col+j*9]] -= 1;
-                        possibility[i][0] -= 1;
-                    }
-                }
-            }
-            for(int j = 0 ; j < 9 ; j++){
-                //check zone
-                if(cell[zone[j]] != 0 && cell[zone[j]] != -1){
-		    if( possibility[i][cell[zone[j]]] == 1){
-                        possibility[i][cell[zone[j]]] -= 1;
-                        possibility[i][0] -= 1;
-                    }
-                }
-            }
-	}
-    }
-}
+
 void Sudoku::Solve_strategy2(){
-    //check row from 1 to 9 if there's only one cell can be filled with the number then fill
-    for(int row = 0 ; row < 9 ; row ++){
-	for(int i = 1 ; i < 10; i ++){
+    //check row
+    for(int x = 0 ; x < 9 ; x ++){
+        for(int i = 1 ; i < 10; i ++){
             int countpossibility = 0;
-     	    for(int j = 0 ; j < 9 ; j ++){
-	        if( possibility[row*9+j][i] == 1) countpossibility += 1;
-	    }
-	    if(countpossibility == 1){
-	        for(int j = 0 ; j < 9 ; j ++){
-		    if( possibility[row*9+j][i] == 1){
-			cell[row*9+j] = i;
-			CheckAfterFill(row*9+j);
-			break;
+            for(int y = 0 ; y < 9 ; y ++){
+                if( possibility[x][y][i] == 1) countpossibility += 1;
+            }
+            if(countpossibility == 1){
+                for(int y = 0 ; y < 9 ; y ++){
+                    if( possibility[x][y][i] == 1){
+                        cell[x][y] = i;
+                        CheckAfterFill(x,y);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    //check col
+    for(int y = 0 ; y < 9 ; y++){
+        for(int i = 1 ; i < 10; i++){
+            int countpossibility = 0;
+            for(int x = 0 ; x < 9 ; x++){
+                if( possibility[x][y][i] == 1) countpossibility += 1;
+            }
+            if(countpossibility == 1){
+                for(int x = 0 ; x < 9 ; x++){
+                    if( possibility[x][y][i] == 1){
+                        cell[x][y] = i;
+                        CheckAfterFill(x,y);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    //check zone
+    for(int x = 0 ; x < 9 ; x+=3){
+        for(int y = 0 ; y < 9 ; y+=3){
+            for(int i = 1 ; i < 10; i++){
+                int countpossibility = 0;
+                for(int n = 0 ; n < 9 ; n++){
+                    int xx = x/3*3+n%3;
+                    int yy = y/3*3+n/3;
+                    if(possibility[xx][yy][i] == 1) countpossibility += 1;
+                }
+                if(countpossibility == 1){
+                    for(int n = 0 ; n < 9 ; n++){
+                    int xx = x/3*3+n%3;
+                    int yy = y/3*3+n/3;
+                    if(possibility[xx][yy][i] == 1)
+                        cell[xx][yy] = i;
+                        CheckAfterFill(xx,yy);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+int Sudoku::backtracking(int board[][9]){
+    if (isSolved(board)){
+	ans++;
+        return paste(Ans, board);
+    }
+    int newboard[9][9];
+    paste(newboard, board);
+
+    for (int x=0;x<9;x++)
+        for (int y=0;y<9;y++)
+            if (newboard[x][y]==0){
+                for (int n=1;n<=9;n++){
+                    newboard[x][y]=n;
+                    if (checkvalid(newboard, x, y)==1){
+                        if (backtracking(newboard))
+                            return 1;
+		    	newboard[x][y]=0;
 		    }
-	    	}
-	    }
-	}
-    }
-    //check col from 1 to 9 if there's only one cell can be filled with the number then fill
-    for(int col = 0 ; col < 9 ; col ++){
-        for(int i = 1 ; i < 10; i ++){
-            int countpossibility = 0;
-            for(int j = 0 ; j < 9 ; j ++){
-                if( possibility[col+j*9][i] == 1) countpossibility += 1;
-            }
-            if(countpossibility == 1){
-                for(int j = 0 ; j < 9 ; j ++){
-                    if( possibility[col+9*j][i] == 1){
-                        cell[col+9*j] = i;
-			CheckAfterFill(col+9*j);
-			break;
-                    }
                 }
+                return 0;
             }
-        }
-    }
-    //check zones from 1 to 9 if there's only one cell can be filled with the number then fill
-    for(int numofzone = 0 ; numofzone < 9 ; numofzone ++){
-        int zone[9] = {0,1,2,9,10,11,18,19,20};
-	for(int i = 0 ; i < 9 ; i++){
-	    zone[i] += numofzone%3*3 + numofzone/3*27;
-	}
-        for(int i = 1 ; i < 10; i ++){
-            int countpossibility = 0;
-            for(int j = 0 ; j < 9 ; j ++){
-                if(possibility[zone[j]][i] == 1) countpossibility += 1;
-            }
-            if(countpossibility == 1){
-                for(int j = 0 ; j < 9 ; j ++){
-                    if(possibility[zone[j]][i] == 1){
-                        cell[zone[j]] = i;
-			CheckAfterFill(zone[j]);
-			break;
-                    }
-                }
-            }
-        }
-    }
-}
-void Sudoku::Check(){
-    for(int i = 0 ; i < sudokuSize ; i++){
-        if(cell[i] == 0){
-            int row = i/9;
-            int col = i%9;
-            int zonerow = i/27;
-            int zonecol = i%9/3;
-            int zone[9] = {0,1,2,9,10,11,18,19,20};
-            for(int j = 0 ; j < 9 ; j++){
-                zone[j] += zonerow * 27 + zonecol * 3;
-            }
-            for(int j = 0 ; j < 9 ; j++){
-                //check row
-                if(cell[row*9+j] != 0){
-		    if(possibility[i][cell[row*9+j]] == 1){
-                        possibility[i][cell[row*9+j]] -= 1;
-                        possibility[i][0] -= 1;
-                    }
-                }
-                //check col
-                if(cell[col+j*9] != 0){
-                    if(possibility[i][cell[col+j*9]] == 1){
-                        possibility[i][cell[col+j*9]] -= 1;
-                        possibility[i][0] -= 1;
-                    }
-                }
-            }
-            for(int j = 0 ; j < 9 ; j++){
-                //check zone
-                if(cell[zone[j]] != 0 && cell[zone[j]] != -1){
-		    if( possibility[i][cell[zone[j]]] == 1){
-                        possibility[i][cell[zone[j]]] -= 1;
-                        possibility[i][0] -= 1;
-                    }
-                }
-            }
-	}
-	else{
-	    for(int j = 0 ; j < 10 ; j ++){
-		possibility[i][j] = 0 ;
-	    }
-	}
-    }
+    return 0;
 }
 
-void Sudoku::CheckAfterFill(int i){    
-    int value = cell[i];
-    int row = i/9;
-    int col = i%9;
-    int zonerow = i/27;
-    int zonecol = i%9/3;
-    int zone[9] = {0,1,2,9,10,11,18,19,20};
-    for(int j = 0 ; j < 9 ; j++){
-        zone[j] += zonerow * 27 + zonecol * 3;
+int Sudoku::checkvalid(int board[][9], int x, int y){
+    int xx, yy;
+    for(int n = 0; n < 9; n++){
+        xx = x/3*3+n%3;
+        yy = y/3*3+n/3;
+        if (x!=xx && y!=yy && board[xx][yy]==board[x][y])
+            return 0;
     }
-    for(int j = 0 ; j < 10 ; j ++){
-	possibility[i][j] = 0;
+    for(int n = 0; n < 9 ;n++){
+        if ((n!=y && board[x][n]==board[x][y]) || (n!=x && board[n][y]==board[x][y]))
+            return 0;
     }
-    for(int j = 0 ; j < 9 ; j++){
-	if( possibility[row*9+j][value] == 1){ 
-	    possibility[row*9+j][value] -= 1;
-	    possibility[row*9+j][0] -= 1;
-	} 
-	if( possibility[col+9*j][value] == 1){
-            possibility[col+9*j][value] -= 1;
-            possibility[col+9*j][0] -= 1;
-	}
-	if( possibility[zone[j]][value] == 1){
-            possibility[zone[j]][value] -= 1;
-            possibility[zone[j]][0] -= 1;
-        }
+    return 1;
+}
 
-    }
-		    
+int Sudoku::ZeroCount(){
+    int zerocount = 0;
+    for(int x = 0 ; x < 9 ; x++)
+        for(int y = 0 ; y < 9 ; y++)
+            if(cell[x][y] == 0) zerocount += 1;
+    return zerocount;
+}
+
+
+int Sudoku::isSolved(int board[][9]){
+    int x, y;
+    for (x = 0;x < 9; x++)
+        for (y = 0; y < 9; y++)
+            if (board[x][y]==0)
+                return 0;
+    return 1;
+}
+
+int Sudoku::paste(int a[][9], int b[][9]){
+    int x,y;
+    for (x = 0; x < 9; x++)
+        for (y = 0; y < 9; y++)
+            a[x][y] = b[x][y];
+    return 1;
 }
 
 void Sudoku::PrintOut(){
-    for(int i = 0 ; i < 9 ; i++ ){
-	for(int j = 0 ; j < 9 ; j ++){
-            cout << setw(3) << cell[i*9+j]; 
-	}
-	cout << endl;
+    cout << ans << endl;
+    if(ans == 1){
+        for(int x = 0 ; x < 9 ; x++){
+            for(int  y = 0 ; y < 9 ; y++){
+                cout << setw(3) << Ans[x][y];
+            }
+            cout << endl;
+        }
     }
-}
-
-void Sudoku::GiveQuestion(){
-    int a,b,c,d,e,f,g,h,i;
-    
-    
 }
--- Sudoku.h
+++ Sudoku.h
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
-
+#include <cstdlib>
+#include <ctime>
 class Sudoku
 {
     public:
         Sudoku();
-        void GiveQuestion();
         void ReadIn();
         void Solve();
-        void Solve_strategy1();
-	void Solve_strategy2();
-	void FirstCheck();
-	void Check();
-	int GetNextZero();
-	int ZeroCount();
-	bool Process();
-	void CheckAfterFill(int i);
-	void PrintOut();
-        static const int sudokuSize = 81;
+        void GiveQuestion();
+    protected:
     private:
-        int cell[sudokuSize];
-        int possibility[sudokuSize][10];
+        void FirstCheck();
+        void CheckAfterFill(int x,int y);
+        void Solving();
+        void Solve_strategy1();
+        void Solve_strategy2();
+        int backtracking(int board[][9]);
+        int checkvalid(int board[][9], int x, int y);
+        int ZeroCount();
+        int isSolved(int board[][9]);
+        int paste(int a[][9], int b[][9]);
+        void PrintOut();
+        int ans;
+        int cell[9][9];
+        int Ans[9][9];
+        int possibility[9][9][10];
 };
-
 
